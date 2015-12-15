ee8890b80f727f4f169444eaf2e9fd4fe8b02345
Jos van der Til <jos@vandertil.eu>
2015-12-15 22:54:31 +0100

    Day 02

---
+++ Day02.01/Program.fs
@@ -0,0 +1,48 @@
+﻿module Program
+
+open System
+open System.IO
+
+type Rectangle = 
+    { length : int32
+      width : int32
+      height : int32 }
+
+let parseInput (line : string) = 
+    let parts = 
+        line.Split('x')
+        |> Seq.map Int32.Parse
+        |> Seq.toArray
+    
+    let rect = 
+        { length = parts.[0]
+          width = parts.[1]
+          height = parts.[2] }
+    
+    rect
+
+let surfaceAreas rect = 
+    [ (rect.length * rect.width)
+      (rect.width * rect.height)
+      (rect.height * rect.length) ]
+
+let getSmallestSideArea areaParts = areaParts |> List.min
+
+let calcSurfaceArea areaParts = 
+    areaParts
+    |> List.sum
+    |> (*) 2
+
+[<EntryPoint>]
+let main argv = 
+    let calcRequiredPaperForPresent areaParts = (calcSurfaceArea areaParts) + (getSmallestSideArea areaParts)
+    
+    let requiredPaper = 
+        File.ReadAllLines("input.txt")
+        |> Seq.map parseInput
+        |> Seq.map surfaceAreas
+        |> Seq.map calcRequiredPaperForPresent
+        |> Seq.sum
+
+    printfn "%i" requiredPaper
+    0
---
+++ Day02.02/Program.fs
@@ -0,0 +1,42 @@
+﻿module Program
+
+open System
+open System.IO
+
+type Rectangle = 
+    { length : int32
+      width : int32
+      height : int32 }
+
+let parseInput (line : string) = 
+    let parts = 
+        line.Split('x')
+        |> Seq.map Int32.Parse
+        |> Seq.toArray
+    
+    let rect = 
+        { length = parts.[0]
+          width = parts.[1]
+          height = parts.[2] }
+    
+    rect
+
+let getFacePerimeters rect = 
+    [ rect.length + rect.height
+      rect.width + rect.length
+      rect.height + rect.width ]
+    |> List.map ((*) 2)
+
+let getVolume rect = rect.length * rect.height * rect.width
+
+[<EntryPoint>]
+let main argv = 
+    let calcRibbonLength rect = (getFacePerimeters rect |> List.min) + (getVolume rect)
+
+    File.ReadAllLines("input.txt")
+    |> Seq.map parseInput
+    |> Seq.map calcRibbonLength
+    |> Seq.sum
+    |> printfn "%i"
+
+    0
