650dcf82c480a7da3b37785b4976664192ad188f
Jos van der Til <jos@vandertil.eu>
2015-12-15 22:13:28 +0100

    Day 01

---
+++ Day01.01/Program.fs
@@ -0,0 +1,20 @@
+﻿module Program
+
+open System.IO
+
+let charToInt char = 
+    match char with
+    | '(' -> 1
+    | ')' -> -1
+    | _ -> 0
+
+[<EntryPoint>]
+let main argv = 
+    let input = File.ReadAllText("input.txt")
+    
+    let floor = 
+        input
+        |> Seq.map charToInt
+        |> Seq.sum
+    printfn "%i" floor
+    0
---
+++ Day01.02/Program.fs
@@ -0,0 +1,29 @@
+﻿module Program
+
+let charToInt char = 
+    match char with
+    | '(' -> 1
+    | ')' -> -1
+    | _ -> 0
+
+let emptyProgress = (false, -1, 0)
+
+let detectBasement progress (index, change) = 
+    let (found, foundIndex, prevFloor) = progress
+    if found then progress
+    else 
+        let curFloor = prevFloor + change
+        if curFloor < 0 then (true, index, curFloor)
+        else (false, -1, curFloor)
+
+[<EntryPoint>]
+let main argv = 
+    let input = System.IO.File.ReadAllText("input.txt")
+    
+    let (_, basement, _) = 
+        input
+        |> Seq.map charToInt
+        |> Seq.indexed
+        |> Seq.fold detectBasement emptyProgress
+    printfn "%i" (basement + 1) // Compensate for zero based indexing in F#
+    0
