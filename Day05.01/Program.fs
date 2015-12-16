14cdf0c2f349e7a737e6d9e9f43cd0e63302c343
Jos van der Til <jos@vandertil.eu>
2015-12-16 17:14:34 +0100

    Day 05

---
+++ Day05.01/Program.fs
@@ -0,0 +1,46 @@
+﻿module Program
+
+open System.IO
+
+let isVowel char = 
+    match char with
+    | 'a' -> true
+    | 'e' -> true
+    | 'i' -> true
+    | 'o' -> true
+    | 'u' -> true
+    | _ -> false
+
+let hasEnoughVowels (input : string) = 
+    let vowelCount =
+        input
+        |> Seq.filter isVowel
+        |> Seq.length
+
+    vowelCount >= 3
+
+let hasDoubleLetter (input : string) = 
+    input
+    |> Seq.pairwise
+    |> Seq.filter (fun (first, second) -> first = second)
+    |> Seq.tryHead
+    |> Option.isSome
+
+let containsNaughtyString (input : string) = 
+    [ "ab"; "cd"; "pq"; "xy" ]
+    |> Seq.filter (fun naughty -> input.Contains(naughty))
+    |> Seq.tryHead
+    |> Option.isSome
+
+[<EntryPoint>]
+let main argv = 
+    let noNaughtyStrings input = not(containsNaughtyString(input))
+
+    File.ReadAllLines("input.txt")
+    |> Seq.filter noNaughtyStrings
+    |> Seq.filter hasDoubleLetter
+    |> Seq.filter hasEnoughVowels
+    |> Seq.length
+    |> printfn "%i"
+
+    0
---
+++ Day05.02/Program.fs
@@ -0,0 +1,26 @@
+﻿module Program
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+let hasSameLetterWithOneSeparator input =
+    let regex = Regex("(.).\1")
+    input |> regex.IsMatch
+
+let hasLetterPair input =
+    let regex = Regex("(..).*\1")
+    input |> regex.IsMatch
+
+let filter input = 
+    input
+    |> Seq.filter hasSameLetterWithOneSeparator
+    |> Seq.filter hasLetterPair
+
+[<EntryPoint>]
+let main argv = 
+    File.ReadAllLines("input.txt")
+    |> filter
+    |> Seq.length
+    |> printfn "%i"
+    0
